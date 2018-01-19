dbe273c99d5064af6a0567adfa9b62d84c0bb5c9
Chad Foley <chad.foley@raleighnc.gov>
2018-01-19 13:15:38 -0500

    add zoom and point on map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Value } from './service-request';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from './user';
 import { Buildings } from './buildings';
 import { CityworksService } from './cityworks.service';
 import { ArcgisService } from './arcgis.service';
-
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
@@ -13,25 +13,32 @@ import { ArcgisService } from './arcgis.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  index: number;
+  @ViewChild(MapComponent) childMapComponent;
+
+  coords: { x: number; y: number; };
+  lindex: number;
+  pindex: number;
   formattedMessage: string;
-
-
+  locations: Array<{
+    descr: string, geometry: {
+      x: number,
+      y: number
+    }
+  }> = [];
+  problems: Array<{ descr: string, problemSid: number }> = [];
+  title = 'app';
+  geometry: {
+    "x": -78.607868672773819,
+    "y": 35.866855800674266
+  };
+  public myForm: FormGroup; // our model driven form
   buildings: Buildings;
+  // mapcomponent: MapComponent;
 
   constructor(private _fb: FormBuilder, private cityworksservice: CityworksService, private arcgisservice: ArcgisService) { }
 
-  // location = "";
-  locations = [];
-  // problems = [];
-  problems: Array<{descr: string, problemSid: number}> = [];
-
-
-  title = 'app';
-  public myForm: FormGroup; // our model driven form
-  x;
-
   ngOnInit() {
+
     this.myForm = this._fb.group({
       callerFirstName: [''],
       callerLastName: ['', [<any>Validators.required]],
@@ -49,52 +56,61 @@ export class AppComponent implements OnInit {
     this.arcgisservice.getFacilities().subscribe(
       data => {
         for (let i = 0; i < data.features.length; i++) {
-          console.log(data.features[i].attributes.LOCATION);
+          // console.log(data.features[i].attributes.LOCATION);
           if (data.features[i].attributes.WEBFORM === 'Y') {
-            this.locations.push(data.features[i].attributes.LOCATION);
+            this.locations.push({ descr: data.features[i].attributes.LOCATION, geometry: data.features[i].geometry} );
           }
         }
-        this.locations.sort();
-        // console.log('location selected = ', this.location);
+        this.locations.sort(function(a, b) {
+          if (a["descr"] < b["descr"]) return -1;
+          if (a["descr"] > b["descr"]) return 1;
+          return 0;
+      });
       },
       err => {
         console.log('some error happened');
-        this.locations.push('Error getting facilities list');
+        this.locations.push({ descr: 'Error getting facilities list', geometry: {x: 0, y: 0} });
       }
     );
 
     this.cityworksservice.getProblemTypes().subscribe(
       data => {
         for (let i = 0; i < data.Value.length; i++) {
-          console.log(data.Value[i].Description);
-          this.problems.push({descr: data.Value[i].Description, problemSid: data.Value[i].ProblemSid});
+          // console.log(data.Value[i].Description);
+          this.problems.push({ descr: data.Value[i].Description, problemSid: data.Value[i].ProblemSid });
         }
         this.problems.sort();
       },
       err => {
         console.log('some error happened');
-        this.problems.push({descr: 'Error getting problems list', problemSid: 0});
+        this.problems.push({ descr: 'Error getting problems list', problemSid: 0 });
       }
     );
-
     this.onChanges();
-
   }
 
   onChanges(): void {
     this.myForm.get('problemCode').valueChanges.subscribe(val => {
-      this.index = this.problems.indexOf(val);
-      console.log('sid is ', this.problems[this.index].problemSid);
+      this.pindex = this.problems.indexOf(val);
+      console.log('sid is ', this.problems[this.pindex].problemSid);
+    });
+
+    this.myForm.get('loc').valueChanges.subscribe(val => {
+      this.lindex = this.locations.indexOf(val);
+      // TODO: pass geometry to map
+      console.log('geometry is ', this.locations[this.lindex].geometry);
+      this.coords = this.locations[this.lindex].geometry;
+      this.childMapComponent.zoom(this.coords);
+
       this.formattedMessage = `My name is ${val.descr}.`;
-      console.log('formattedMessage = ',this.formattedMessage);
-    });  
+      console.log('formattedMessage = ', this.formattedMessage);
+    });
   }
 
   save(model: User, isValid: boolean) {
 
-    // delete model.srStatus;
-    // move callerAddress to address to display in Cityworks in both fields
-    model.address = this.myForm.get('callerAddress').value;
+    model.address = this.myForm.get('callerEmail').value;
+    model.problemSid = this.myForm.get('problemCode').value;
 
     console.log('model is ', model, isValid);
     console.log('stringified model', JSON.stringify(model));
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 
 import { loadModules } from 'esri-loader';
+import { Buildings } from '../buildings';
 
 @Component({
   selector: 'app-map',
@@ -8,24 +9,24 @@ import { loadModules } from 'esri-loader';
   styleUrls: ['./map.component.css']
 })
 
-
 export class MapComponent implements OnInit {
 
   map: __esri.Map;
   mapView: __esri.MapView;
+  pointGraphic: __esri.Graphic;
+  markerSymbol: __esri.SimpleMarkerSymbol;
 
   @ViewChild('map') mapEl: ElementRef;
 
   @Input() mapProperties: __esri.MapProperties;
   @Input() mapViewProperties: __esri.MapViewProperties = {};
-  
+
   // this is needed to be able to create the MapView at the DOM element in this component
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
 
   constructor() { }
 
   public ngOnInit() {
-
     return loadModules([
       'esri/Map',
       'esri/views/MapView'
@@ -51,5 +52,44 @@ export class MapComponent implements OnInit {
       .catch(err => {
         console.log(err);
       });
-  } 
+  }
+
+  public zoom(coords) {
+
+    loadModules([
+      'esri/Graphic',
+      'esri/geometry/Point',
+      'esri/symbols/SimpleMarkerSymbol'
+    ]).then(([Graphic, Point, SimpleMarkerSymbol]) => {
+
+      this.markerSymbol = new SimpleMarkerSymbol({
+        color: [226, 119, 40],
+        outline: { // autocasts as new SimpleLineSymbol()
+          color: [255, 255, 255],
+          width: 2
+        }
+      });
+
+      this.pointGraphic = new Graphic({
+        geometry: new Point({
+          longitude: coords.x,
+          latitude: coords.y
+        })
+      });
+
+      this.pointGraphic.symbol = this.markerSymbol;
+      this.mapView.graphics.removeAll();
+      this.mapView.graphics.add(this.pointGraphic);
+
+    }).catch(err => {
+      console.log(err);
+    });
+
+    this.mapView.goTo({
+      center: [coords.x, coords.y],
+      zoom: 17
+    });
+
+  }
+
 }
